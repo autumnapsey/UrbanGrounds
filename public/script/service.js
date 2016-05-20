674145fd5045e80546f7bd303f5c63efe461128e
Autumn Apsey <autumnapsey@gmail.com>
2016-05-20 14:29:01 -0400

    API ajax call working for Google Places

--- public/script/service.js
+++ public/script/service.js
@@ -1,7 +1,7 @@
 var app = angular.module('app');
 
-app.factory('coffeeService', function(){
-	return 'Service is speaking!';
-});
+app.factory('coffeeService', function($http){
+	return $http.get(
+"https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJtzwfLTItO4gRxwpKgcgFomE&key=AIzaSyB3qOjxMH_B5zFUt9V4KKU0_MgXaXDd26o")
 
-// test line
+});
