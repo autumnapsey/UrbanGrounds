3ef54da16b58168713ea355b2ec8abf2a26d4957
Veronica Williams <veronica.robin.williams@gmail.com>
2016-05-21 00:12:55 -0400

    filter experiment

--- public/script/selectCtrl.js
+++ public/script/selectCtrl.js
@@ -1,6 +1,35 @@
-var app = angular.module('app');
+var app = angular.module('app',);
 
 app.controller('selectCtrl', function($scope, coffeeService) {
 	console.log('hello from the selectCtrl!');
 	console.log(coffeeService);
 });
+
+
+
+
+//var myApp = angular.module('myApp',[]);
+
+//filter functions below-Veronica
+
+myApp.filter('exactMatch', function() {
+    return function(shops, pattern) {
+        var result = [];
+        shops.forEach(function (shop) {
+            if (shop.goodfor === pattern) {
+                result.push(shop);
+            }
+        });                
+        return result;
+    }
+});
+
+function MyCtrl($scope) {
+    $scope.shops = [
+        {name: "UB", goodfor: "music"},
+        {name: "Starb", goodfor: "work"},
+        {name: "Grindhouse", goodfor: "music"},
+        {name: "coffee", goodfor: "studying"},
+        {name: "ashe coffee supply", goodfor: "friends"},
+    ];
+}
