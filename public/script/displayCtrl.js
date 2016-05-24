e1769634c995ad90f8147e33de11c9587e5d30e4
Autumn Apsey <autumnapsey@gmail.com>
2016-05-24 09:44:36 -0400

    merged conflicts and formated coffee page

--- public/script/displayCtrl.js
+++ public/script/displayCtrl.js
@@ -4,5 +4,12 @@ app.controller('displayCtrl', function($scope, coffeeService) {
 	coffeeService.getLink().then(function(location){
 		$scope.coffeeShop = location.data.result;
 		console.log(location);
+		$scope.openNow = function(){
+			if(location.data.result.opening_hours.open_now === true){
+				return "Open Now";
+			} else {
+				return "Currently Closed";
+			}
+		};
 	});
 });
--- public/script/selectCtrl.js
+++ public/script/selectCtrl.js
@@ -1,50 +1,19 @@
 var app = angular.module('app');
 
-<<<<<<< HEAD
-=======
-
-app.controller('selectCtrl', function($scope, coffeeService) {
-	console.log('hello from the selectCtrl!');
-	console.log(coffeeService);
-});
-
-
->>>>>>> 7d329efc0b19394d9314f508aaf8ccdb29b2fe58
-
 app.controller('selectCtrl', function($scope, coffeeService, $location) {
 	console.log('Hi from selectCtrl');
 	$scope.chooseShop = function (num){
-		console.log(num);
 		coffeeService.getShop(num);
 		$location.path('/coffeeshop')
 	};
+
 	$scope.downtownList = false;
 	$scope.midtownList = false;
 	$scope.newcenterList = false;
 	$scope.corktownList = false;
-<<<<<<< HEAD
-
-});
-=======
 	$scope.westVillageList = false;
 	$scope.jeffersonList = false;
 	$scope.hamtramckList = false;
-	$scope. easternMarketList = false;
+	$scope.easternMarketList = false;
 	$scope.rosedaleParkList = false;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 7d329efc0b19394d9314f508aaf8ccdb29b2fe58
