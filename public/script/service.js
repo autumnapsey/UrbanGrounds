f97ce575dcc9fcdf770e28ec451477402bd70c16
Rose Ryan Campau <rcampau@icloud.com>
2016-05-26 13:23:01 -0400

    updated accurate boolean values for all cafes, also added a couple of cafes and reduced neighbourhoods

--- public/script/service.js
+++ public/script/service.js
@@ -11,23 +11,31 @@ app.factory('coffeeService', function($http){
 				work: true,
 				food: true,
 				social: true,
-				art: true
+				music: false,
+				all: true
 			},
 			{
 				name: 'Urban Bean Co.',
 				placeID: 'ChIJt7odBjEtO4gRgRETxl3QoLE',
 				description: 'The Urban Bean is a hip haven featuring mid-century modern style and DJs spinning techno and house. It’s got a second level providing ample space for you and your friends.',
 				features: '',
-				music: true,
 				work: true,
-				social: true
+				social: true,
+				food: false,
+				music: true,
+				all: true
+
 			},
 			{
 				name: 'Roasting Plant',
 				placeID: 'ChIJUwca7i8tO4gRC6azALL0VXY',
 				description: 'The future is now when it comes to coffee at the Roasting Plant. Boasting a Jetsons worthy Javabot that brews your coffee, the Roasting Plant offers novelty and a good cup of coffee.',
 				features: '',
-// needs categories
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
 
 			},
 			{
@@ -35,25 +43,46 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJs1AnRCUtO4gRIazc_Vjj-ss',
 				description: 'Go to see the inside of the fabulous mosaic lobby of the Guardian Building, stay for the tasty coffee at the Rowland Cafe. Located in one of the most stunning buildings in Detroit, it’s a great place to visit and have a light lunch.',
 				features:
-				work: true,
-				food: true
+				work: false,
+				social: false,
+				food: true,
+				music: false,
+				all: true
 			},
 			{
 				name: 'Dessert Oasis',
 				placeID: 'ChIJzZVcTTAtO4gRe5sIIK-MgJs',
 				description: 'As the name implies, Dessert Oasis’ sweet treats are a welcome bonus to this Capital Park coffee shop. With ample seating options, and industrial atmosphere, it’s a great place to get to work.',
 				features: '',
+				work: true,
 				social: true,
-				food: true,
-				social: true
+				food: false,
+				music: false,
+				all: true
 			},
 			{
 				name: 'The Bean Bar',
 				placeID: 'ChIJdyYY-zotO4gRSkl3yFB0L4U',
 				description: 'The Bean Bar serves coffee, smoothies and light fare to workers in the Cobo area. You can’t miss it with it’s space-age sign. The hours are strictly breakfast and lunch, so don’t go looking for an evening pick-me-up.',
 				features: '',
+				work: true,
+				social: true,
 				food: true,
-				social: true
+				music: false,
+				all: true
+			}
+			{
+				name: 'Detroit Athletic Club Cafe',
+				placeID: 'ChIJddBmfDItO4gRRlJjD-hy6cc',
+				description: '',
+				features: '',
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
+
+
 			}
 		],
 		midtown: [
@@ -62,44 +91,66 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJOZ_JV6XSJIgRuE7W1bI52bY',
 				description: '',
 				features: '',
+				work: false,
 				social: true,
+				food: true,
 				music: true,
-				wrk: true,
-				art: true
+				all: true
 			},
 			{
 				name: 'Fourteen East',
 				placeID: 'ChIJ8Vn3cL3SJIgRAN16yJ9YXVc',
 				description: '',
 				features: '',
+				work: true,
+				social: true,
 				food: true,
-				work: true
+				music: false,
+				all: true
 			},
 			{
 				name: 'Great Lakes Coffee',
 				placeID: 'ChIJi82DCLjSJIgRVh34r-ayWs0',
 				description: '',
 				features: '',
-				food: true,
 				work: true,
-				social: true
+				social: true,
+				food: true,
+				music: false,
+				all: true
+
 			},
 			{
 				name: 'Avalon',
 				placeID: 'ChIJG1njurDSJIgRQOyfZl8DS3U',
 				description: '',
 				features: '',
+				work: false,
+				social: true,
 				food: true,
-				social: true
+				music: false,
+				all: true
 			},
 			{
 				name: 'SoCRA Tea',
 				placeID: 'ChIJKZ7aELnSJIgRxwOrLDpqooQ',
 				description: '',
 				features: '',
-				food: true,
 				work: true,
-				social: true
+				social: true,
+				food: true,
+				music: false,
+				all: true
+			}
+			{
+				name: 'Kresge Court'
+				placeID: '',
+				features: '',
+				work: true,
+				social: true,
+				food: true,
+				music: true,
+				all: true
 			}
 		],
 		newCenter:[
@@ -108,16 +159,22 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJKZ7aELnSJIgRxwOrLDpqooQ',
 				description: '',
 				features: '',
-				food: true,
+				work: true,
 				social: true,
-				work: true
+				food: true,
+				music: false,
+				all: true
 			},
 			{
 				name: 'Stella International Cafe',
 				placeID: 'ChIJKZ7aELnSJIgRxwOrLDpqooQ',
 				description: '',
 				features: '',
-// needs categories
+				work: false,
+				social: true,
+				food: false,
+				music: false,
+				all: true
 			}
 		],
 		corktown:[
@@ -126,6 +183,11 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJQZSTxVktO4gR_qjDkKNYauk',
 				description: '',
 				features: ''
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
 // needs categories
 			},
 			{
@@ -133,6 +195,11 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJp-Jg8VwtO4gRJKYZBSShFTg',
 				description: '',
 				features: ''
+				work: true,
+				social: true,
+				food: false,
+				music: false,
+				all: true
 // needs categories
 			}
 		],
@@ -142,17 +209,34 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJY2gfQ17TJIgRCC-Ib8eakH8',
 				description: '',
 				features: ''
-// needs categories
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
 
-			}
-		],
-		jefferson: [
+				{
+					name: 'Sister Pie',
+					placeID: 'ChIJucNILWfTJIgRUonGOtN2O7Y',
+					description: '',
+					work: false,
+					social: true,
+					food: true,
+					music: false,
+					all: true
+				}
+
 			{
 				name: 'Coffee and (___)',
 				placeID: 'ChIJ6eXdWAfVJIgRw_BSGXWh7gs',
 				description: '',
 				features: ''
-// needs categories
+				work: false,
+				social: true,
+				food: true,
+				music: false,
+				all: true
+
 			}
 		],
 		easternMarket: [
@@ -161,14 +245,33 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJw-es3tXSJIgRnzz7A_tsOOQ',
 				description: '',
 				features: ''
-// needs categories
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
 			},
+			{
+				name: 'Cairo Coffee'
+				placeID: 'ChIJQYrmDdfSJIgRmCFB7UeQu4Q',
+				description: '',
+				features: '',
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
+			}
 			{
 				name: 'Germack Coffee Roasting Co.',
 				placeID: 'ChIJZS3wv9bSJIgRMcmDKePqdLQ',
 				description: '',
 				features: ''
-// needs categories
+				work: false,
+				social: true,
+				food: false,
+				music: false,
+				all: true
 			}
 		],
 		rosedalePark: [
@@ -177,8 +280,13 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJ5aZkHGrKJIgR7-jlmpGsSH4',
 				description: '',
 				features: ''
-// needs categories
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
 			}
+
 		],
 		hamtramck: [
 			{
@@ -186,7 +294,22 @@ app.factory('coffeeService', function($http){
 				placeID: 'ChIJHS1-WWjSJIgR9OXqeGytqE0',
 				description: '',
 				features: ''
-// needs categories
+				work: true,
+				social: true,
+				food: true,
+				music: false,
+				all: true
+			}
+			{
+				name: 'Caniff Deli Coffee LLC',
+				placeID: 'ChIJbSgRrm3SJIgRGGuc10CluBQ',
+				description: '',
+				features: '',
+				work: false,
+				social: true,
+				food: true,
+				music: true,
+				all: true
 			}
 		]
 	};
