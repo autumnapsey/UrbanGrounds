cffbb69fcc80c1dd841d1f18ed98b3f89a02d409
Rose Ryan Campau <rcampau@icloud.com>
2016-06-01 09:55:23 -0400

    trying to get angular click functions to work for the neighbourhood icons

--- public/script/selectCtrl.js
+++ public/script/selectCtrl.js
@@ -24,8 +24,11 @@ app.controller('selectCtrl', function($scope, coffeeService, $location) {
 		$scope.places = coffeeService.selectCategory(interest);
 	};
 
+
+
+
 	$scope.class = "icon";
-	$scope.changeClass = function(event){
+	$scope.changeInterestClass = function(event){
 		console.log(event);
 		var element = event.target;
 		if(isObjectEmpty(lastElement)){
@@ -39,6 +42,28 @@ app.controller('selectCtrl', function($scope, coffeeService, $location) {
 
 	function isObjectEmpty(element) {
 		return Object.keys(lastElement).length > 0;
+	};
+
+
+
+
+	$scope.neighbourhoodClass = "neighbourhoodIcon";
+	$scope.changeNeighbourhoodClass = function(event){
+		console.log(event);
+		var neighbourhoodElement = event.target;
+		if(isNeighbourhoodObjectEmpty(lastNeighbourhoodElement)){
+			$(lastNeighbourhoodElement).removeClass("chosenNeighbourhoodIcon").addClass("neighbourhoodIcon");
+		}
+
+		$(neighbourhoodElement).addClass("chosenNeighbourhoodIcon");
+
+		lastNeighbourhoodElement = event.target;
+
 	}
+
+	function isNeighbourhoodObjectEmpty(neighbourhoodElement) {
+		return Object.keys(lastneighbourhoodElement).length > 0;
+	}
+
 
 });
