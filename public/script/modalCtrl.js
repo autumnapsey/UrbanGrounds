01cb4afe9e91b8ecf6bca4c4e1e3bebe37144add
Autumn Apsey <autumnapsey@gmail.com>
2016-06-02 22:36:42 -0400

    moved out unused files from public/ and shortened Autumn bio so the three are close in length

--- public/script/modalCtrl.js
+++ public/script/modalCtrl.js
@@ -1,6 +1,18 @@
 var app = angular.module('app');
 
 app.controller('modalCtrl', function($scope, coffeeService, ngDialog, $location){
+
+	$(window).load(function() {
+		$('.button').raptorize();
+	});
+
+// Konami code doesn't want to work
+	// $(window).load(function() {
+	// 	$('.button').raptorize({
+	// 		'enterOn' : 'konami-code'
+	// 	});
+	// });
+
     $scope.clickForAbout = function (){
         ngDialog.open({
             template: '../view/about.html',
